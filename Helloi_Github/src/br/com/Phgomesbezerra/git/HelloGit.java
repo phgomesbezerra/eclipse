0743d0c8996a88d493a4ab6cf17ca802c615a364
phgomesbezerra <134980814+phgomesbezerra@users.noreply.github.com>
2024-02-12 21:46:49 -0300

    Add files via upload
    adicionei arquivos via upload

---
+++ Helloi_Github/src/br/com/Phgomesbezerra/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.Phgomesbezerra.git;
+
+/**
+ * @author Phgomesbezerra
+ * Exemplo de integraçao com Eclipse com GitHub
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		
+	}
+
+}
